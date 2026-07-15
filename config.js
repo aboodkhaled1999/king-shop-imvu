dd8a6d2e33fff68397e5e4d9bea4570aecd91579
aboodkhaled1999 <aboodkhaled208@gmail.com>
2026-07-15 06:51:02 +0300

    Update KING_CONFIG with additional site details
    Added creatorId and site information to KING_CONFIG.

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
-/* All external links used by KING SHOP live here. */
 window.KING_CONFIG = {
-  storeName: 'KING SHOP',
-  username: 'mmmm511722',
-  urls: {
-    profile: 'https://ar.imvu.com/next/av/mmmm511722/',
-    products: 'https://www.imvu.com/shop/web_search.php?manufacturers_id=315504714'
-  }
+    storeName: "KING SHOP",
+
+    username: "mmmm511722",
+
+    creatorId: "315504714",
+
+    urls: {
+        profile: "https://ar.imvu.com/next/av/mmmm511722/",
+        products: "https://www.imvu.com/shop/web_search.php?manufacturers_id=315504714"
+    },
+
+    site: {
+        title: "KING SHOP",
+        description: "Official IMVU Shop",
+        language: "en"
+    }
 };
